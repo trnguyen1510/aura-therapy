f26f3ec995e2304f8756be86da2653b9f87b300f
Crueldad <55165234+Crueldad@users.noreply.github.com>
2021-03-27 13:36:51 -0700

    personal_info app

--- aura/settings.py
+++ aura/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'personal_info',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- personal_info/urls.py
+++ personal_info/urls.py
@@ -1,4 +1,4 @@
-  from django.urls import path
+from django.urls import path
 from django.conf.urls import include, url, handler404, handler500
 from . import views
 from django.conf import settings
