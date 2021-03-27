aa4d9e99425ad3b07bfff76df1de0156daa5e71a
trnguyen1510 <trnguyen1510@gmail.com>
2021-03-27 14:52:07 -0700

    welcome page is doen

--- aura/urls.py
+++ aura/urls.py
@@ -15,7 +15,18 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import include
+from django.views.generic import RedirectView
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('welcome.urls')),
 ]
+
+
+urlpatterns += [path('', RedirectView.as_view(url='', permanent=True)), ]
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
